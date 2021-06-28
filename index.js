815c2400def85bffac1062c96acc3ba06b1deb7c
TheSteinHaus <semen.skopin@gmail.com>
2021-06-28 13:48:15 +1000

    done

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 function changeTheme(check) {
-    let chbox;
+    let chbox, resolution, chbox1;
+    resolution = window.innerWidth;
     chbox = document.getElementById('toggler');
+    chbox1 = document.getElementById('toggler1');
+    // alert(resolution)
 
-    if (chbox.checked) {
-        th = 'dark';
-        // document.querySelector("#theme > option[selected]").removeAttribute("selected");
-        // document.querySelector("#theme > option[value='dark']").setAttribute("selected", "selected");
+    if (resolution < 1000) {
+        if (chbox.checked) {
+            th = 'dark';
+            document.querySelector("#theme1 > option[selected]").removeAttribute("selected");
+            document.querySelector("#theme1 > option[value='dark']").setAttribute("selected", "selected");
+            document.querySelector("#theme > option[selected]").removeAttribute("selected");
+            document.querySelector("#theme > option[value='dark']").setAttribute("selected", "selected");
+            
+        }
+        else {
+            th = 'light';
+            document.querySelector("#theme1 > option[selected]").removeAttribute("selected");
+            document.querySelector("#theme1 > option[value='light']").setAttribute("selected", "selected");
+            document.querySelector("#theme > option[selected]").removeAttribute("selected");
+            document.querySelector("#theme > option[value='light']").setAttribute("selected", "selected");
+        }
     }
     else {
-        th = 'light';
-        // document.querySelector("#theme > option[selected]").removeAttribute("selected");
-        // document.querySelector("#theme > option[value='light']").setAttribute("selected", "selected");
+        if (chbox1.checked) {
+            th = 'dark';
+            document.querySelector("#theme1 > option[selected]").removeAttribute("selected");
+            document.querySelector("#theme1 > option[value='dark']").setAttribute("selected", "selected");
+            document.querySelector("#theme > option[selected]").removeAttribute("selected");
+            document.querySelector("#theme > option[value='dark']").setAttribute("selected", "selected");
+        }
+        else {
+            th = 'light';
+            document.querySelector("#theme1 > option[selected]").removeAttribute("selected");
+            document.querySelector("#theme1 > option[value='light']").setAttribute("selected", "selected");
+            document.querySelector("#theme > option[selected]").removeAttribute("selected");
+            document.querySelector("#theme > option[value='light']").setAttribute("selected", "selected");
+        }
     }
+    
     check = th;
 
     document.body.classList.remove("theme-light", "theme-dark");
